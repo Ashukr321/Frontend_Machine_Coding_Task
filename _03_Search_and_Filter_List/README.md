b035a66a84a47dfe346b16c296ae79c716596cf5
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-10-03 13:21:28 +0530

    feat ( add search and filter ) features

---
+++ _03_Search_and_Filter_List/README.md
@@ -0,0 +1,30 @@
+### **Search & Filter List**
+
+- Given a list of items (users/products/emails), add search functionality.
+- Add filters (e.g., by category, price, status).
+- Show “no results found” if nothing matches.
+
+
+<!--
+  1. form 
+    search bar 
+    filter options 
+      1. categories
+      2. price
+      3. status 
+    show message no Results Found  
+ -->
+
+<!--  sample product data  -->
+ <!-- const products = [
+  { id: 101, name: "Laptop", category: "Electronics", price: 50000, status: "available", brand: "Dell", rating: 4.5 },
+  { id: 102, name: "Smartphone", category: "Electronics", price: 20000, status: "sold out", brand: "Samsung", rating: 4.2 },
+  { id: 103, name: "T-Shirt", category: "Clothing", price: 500, status: "available", brand: "Levis", rating: 4.0 },
+  { id: 104, name: "Coffee Mug", category: "Kitchen", price: 300, status: "available", brand: "Crockery", rating: 4.1 },
+  { id: 105, name: "Headphones", category: "Electronics", price: 1500, status: "available", brand: "Sony", rating: 4.3 },
+  { id: 106, name: "Refrigerator", category: "Appliances", price: 35000, status: "available", brand: "LG", rating: 4.6 },
+  { id: 107, name: "Jeans", category: "Clothing", price: 1200, status: "sold out", brand: "Wrangler", rating: 4.0 },
+  { id: 108, name: "Blender", category: "Kitchen", price: 2000, status: "available", brand: "Philips", rating: 4.2 },
+  { id: 109, name: "Tablet", category: "Electronics", price: 25000, status: "available", brand: "Apple", rating: 4.7 },
+  { id: 110, name: "Sneakers", category: "Footwear", price: 2500, status: "available", brand: "Nike", rating: 4.4 }
+]; -->
---
+++ _03_Search_and_Filter_List/src/App.jsx
@@ -0,0 +1,105 @@
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
+  const [rating, setRating] = useState("All");
+
+  // data 
+  const products = [
+    { id: 101, name: "Laptop", category: "Electronics", price: 50000, status: "available", brand: "Dell", rating: 4.5 },
+    { id: 102, name: "Smartphone", category: "Electronics", price: 20000, status: "sold out", brand: "Samsung", rating: 4.2 },
+    { id: 103, name: "T-Shirt", category: "Clothing", price: 500, status: "available", brand: "Levis", rating: 4.0 },
+    { id: 104, name: "Coffee Mug", category: "Kitchen", price: 300, status: "available", brand: "Crockery", rating: 4.1 },
+    { id: 105, name: "Headphones", category: "Electronics", price: 1500, status: "available", brand: "Sony", rating: 4.3 },
+    { id: 106, name: "Refrigerator", category: "Appliances", price: 35000, status: "available", brand: "LG", rating: 4.6 },
+    { id: 107, name: "Jeans", category: "Clothing", price: 1200, status: "sold out", brand: "Wrangler", rating: 4.0 },
+    { id: 108, name: "Blender", category: "Kitchen", price: 2000, status: "available", brand: "Philips", rating: 4.2 },
+    { id: 109, name: "Tablet", category: "Electronics", price: 25000, status: "available", brand: "Apple", rating: 4.7 },
+    { id: 110, name: "Sneakers", category: "Footwear", price: 2500, status: "available", brand: "Nike", rating: 4.4 }
+  ];
+
+
+  // console.log(search, category, rating);
+  //  get FilterProduct 
+  const filterProducts = products
+    .filter((prd) => prd.name.toLowerCase().includes(search.toLowerCase()))
+    .filter(prd => category === "All" || prd.category.toLowerCase() === category.toLowerCase())
+    .filter((prd) => rating === "All" || Math.floor(prd.rating) >= Number(rating))
+
+
+  console.log(filterProducts);
+
+  return (
+    <>
+      <h1 className='title'>Search and Filter </h1>
+
+      <div className="container">
+        {/* search */}
+        <div className='form_container'>
+          <form style={{
+            display: "flex",
+            gap: "10px"
+          }}>
+            <div style={{ flex: 1 }}>
+              <input type="text" name='search' value={search} onChange={(e) => { setSearch(e.target.value) }} placeholder='search' />
+            </div>
+            <div style={{ display: "flex", gap: "4px" }}>
+              {/* category filter */}
+              <label>
+                <span >Category:</span>
+                <select value={category} onChange={(e) => { setCategory(e.target.value) }}>
+                  <option value="All">ALL</option>
+                  <option value="Electronics">Electronics</option>
+                  <option value="Clothing">Clothing</option>
+                  <option value="Kitchen">Kitchen</option>
+                  <option value="Footwear">Footwear</option>
+                </select>
+              </label>
+
+              {/* rating filter */}
+              <label >
+                <span> Rating:</span>
+                <select onChange={(e) => { setRating(e.target.value) }}>
+                  <option value="rating" >All</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+              </label>
+
+            </div>
+          </form>
+        </div>
+
+
+        {/* displayData */}
+
+        <div className='products'>
+          {
+            filterProducts.length > 0 ? (
+              filterProducts.map((prd) => (
+                <div key={prd.id} className='product_card'>
+                  <h3>{prd.name}</h3>
+                  <p>Category: {prd.category}</p>
+                  <p>Price: ₹{prd.price}</p>
+                  <p>Brand: {prd.brand}</p>
+                  <p>Rating: {prd.rating}</p>
+                </div>
+
+              ))
+            ) : (
+              <p>No Product Found</p>
+            )
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
